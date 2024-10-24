b2c4d78d889b831db6b40a6b5cbf49abe3496eff
Jim Wild <james.wild6@nhs.net>
2024-10-24 10:27:09 +0000

    Add dockerless scan to the docker action

--- dockerfiles/nhsd-git-secrets-entrypoint.sh
+++ dockerfiles/nhsd-git-secrets-entrypoint.sh
@@ -6,20 +6,18 @@ if [ ! -d /src ]; then
   exit 1
 fi
 
-make install
-
 # Change to the mounted source directory, or fail
 cd /src || exit 1
 
-# Ensure a .gitallowed file exists
-if [ -f .gitallowed ]; then
-  cp .gitallowed /secrets-scanner/.gitallowed
-else
-  echo "Warning: .gitallowed file not found in /src."
+# If there's no .gitallowed file, create an empty one
+if [ ! -f .gitallowed ]; then
+  echo "Creating empty .gitallowed file..."
+  echo "./nhsd-rules-deny.txt" >> .gitallowed
 fi
 
 # Initialize git repo if not already a repo
 if [ ! -d .git ]; then
+  echo "Initializing git repository..."
   git init
 fi
 
@@ -29,11 +27,3 @@ git config --global --add safe.directory /src
 # Run the secrets scan with user-provided arguments (default is scanning all files)
 echo "Running secrets scan..."
 /secrets-scanner/git-secrets "$@"
-
-# Check the exit code of the scan to confirm success or failure
-if [ $? -eq 0 ]; then
-  echo "Secrets scan completed successfully."
-else
-  echo "Secrets scan failed. Review the logs for details."
-  exit 1
-fi
